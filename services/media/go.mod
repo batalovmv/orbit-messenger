batalovmv/orbit-messenger

commit e475d2d6277f00a320444fc999f7b6828696de45

fix: pgx v5.9.1 -> v5.7.3 (requires go 1.25, we need 1.24)

services/media/go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/disintegration/imaging v1.6.2
 	github.com/gofiber/fiber/v2 v2.52.12
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgx/v5 v5.9.1
+	github.com/jackc/pgx/v5 v5.7.3
 	github.com/mst-corp/orbit/pkg v0.0.0-00010101000000-000000000000
 	github.com/nats-io/nats.go v1.37.0
 	github.com/redis/go-redis/v9 v9.7.3

pkg/go.mod
@@ -4,7 +4,7 @@ go 1.24.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.6
-	github.com/jackc/pgx/v5 v5.9.1
+	github.com/jackc/pgx/v5 v5.7.3
 	github.com/prometheus/client_golang v1.23.2
 	github.com/valyala/fasthttp v1.58.0
 )

services/ai/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/alicebob/miniredis/v2 v2.37.0
 	github.com/gofiber/fiber/v2 v2.52.12
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgx/v5 v5.9.1
+	github.com/jackc/pgx/v5 v5.7.3
 	github.com/mst-corp/orbit/pkg v0.0.0
 	github.com/redis/go-redis/v9 v9.18.0
 	github.com/valyala/fasthttp v1.58.0

services/auth/go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.52.12
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgx/v5 v5.9.1
+	github.com/jackc/pgx/v5 v5.7.3
 	github.com/mst-corp/orbit/pkg v0.0.0
 	github.com/pquerna/otp v1.4.0
 	github.com/redis/go-redis/v9 v9.7.3

services/bots/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/alicebob/miniredis/v2 v2.37.0
 	github.com/gofiber/fiber/v2 v2.52.12
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgx/v5 v5.9.1
+	github.com/jackc/pgx/v5 v5.7.3
 	github.com/nats-io/nats.go v1.50.0
 	github.com/redis/go-redis/v9 v9.18.0
 )

services/calls/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/gofiber/contrib/websocket v1.3.2
 	github.com/gofiber/fiber/v2 v2.52.12
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgx/v5 v5.9.1
+	github.com/jackc/pgx/v5 v5.7.3
 	github.com/mst-corp/orbit/pkg v0.0.0
 	github.com/nats-io/nats.go v1.37.0
 	github.com/pion/rtcp v1.2.15

services/integrations/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/alicebob/miniredis/v2 v2.37.0
 	github.com/gofiber/fiber/v2 v2.52.12
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgx/v5 v5.9.1
+	github.com/jackc/pgx/v5 v5.7.3
 	github.com/mst-corp/orbit/pkg v0.0.0-00010101000000-000000000000
 	github.com/redis/go-redis/v9 v9.18.0
 )

services/messaging/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/alicebob/miniredis/v2 v2.37.0
 	github.com/gofiber/fiber/v2 v2.52.12
 	github.com/google/uuid v1.6.0
-	github.com/jackc/pgx/v5 v5.9.1
+	github.com/jackc/pgx/v5 v5.7.3
 	github.com/meilisearch/meilisearch-go v0.36.1
 	github.com/mst-corp/orbit/pkg v0.0.0
 	github.com/nats-io/nats.go v1.37.0

commit 35bb1d3dfd1e63c27e37e59ae610b0efc8a99ddf

fix: revert media go.mod to 1.24.0 (regression after e475d2d)

services/media/go.mod
@@ -1,6 +1,6 @@
 module github.com/mst-corp/orbit/services/media
 
-go 1.25.0
+go 1.24.0
 
 replace github.com/mst-corp/orbit/pkg => ../../pkg
 